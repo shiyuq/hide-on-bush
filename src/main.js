31edfb06ed852eb631a5dd096e5e03f3e258a811
shiyuq <1543082564@qq.com>
2020-09-16 10:39:05 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css' // or 'ant-design-vue/dist/antd.less'
+
 Vue.config.productionTip = false
+
+Vue.use(Antd)
 
 new Vue({
   router,
