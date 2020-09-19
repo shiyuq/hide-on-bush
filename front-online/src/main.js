cd92a775eb1ebb219d3a2384e6959cefa9c45bc4
shiyuq <1543082564@qq.com>
2020-09-19 19:05:49 +0800

    ✨ local loading

--- front-online/src/main.js
+++ front-online/src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css' // or 'ant-design-vue/dist/antd.less'
+import { removeLoadingAnimate } from '@/utils/util'
 
 Vue.config.productionTip = false
 
@@ -13,5 +14,8 @@ Vue.use(Antd)
 new Vue({
   router,
   store,
+  created () {
+    removeLoadingAnimate('preloadingWrapper')
+  },
   render: h => h(App)
 }).$mount('#app')
