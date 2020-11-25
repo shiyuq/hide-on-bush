33685ce31cbeb7b1aaafac4c7c6d38fdcfbbad42
shiyuq <1543082564@qq.com>
2020-11-25 19:17:49 +0800

    🎨 scss update

--- front-online/src/components/home/GlobalNavigation.vue
+++ front-online/src/components/home/GlobalNavigation.vue
@@ -1,9 +1,9 @@
 <template>
-  <header class="home-nav home-logo">
+  <header class="header-home">
     <div class="header-box__nav">
-      <a class="header-box-logo-link___1Ffut" href="/">
-        <div class="header-box-logo___24G68">
-          <img src="https://gw.alipayobjects.com/mdn/rms/afts/img/A*yc04S4iiJI8AAAAAAAAAAAAAARQnAQ" alt="logo">
+      <a class="header-box-logo__link" href="/">
+        <div class="header-box-logo__picture">
+          <img src="@/assets/home/logo/black-logo.png" alt="logo">
         </div>
       </a>
     </div>
@@ -21,12 +21,9 @@ export default {
 }
 </script>
 
-<style scoped>
-.home-nav .home-logo {
+<style scoped lang="scss">
+.header-home {
   background-color: transparent;
-}
-
-.home-logo {
   position: fixed;
   top: 0;
   left: 0;
@@ -36,32 +33,27 @@ export default {
   overflow: visible;
   transition: all 3s cubic-bezier(.645,.045,.355,1);
   will-change: transform;
-}
-
-.header-box__nav {
-  display: flex;
-  height: 100%;
-  padding: 0 16px;
-  box-sizing: border-box;
-  max-width: 1350px;
-  margin-right: auto;
-  margin-left: auto;
-}
-
-.home-logo .header-box-logo-link___1Ffut {
+  .header-box__nav {
+    display: flex;
+    height: 100%;
+    padding: 0 16px;
+    box-sizing: border-box;
+    max-width: 1350px;
+    margin-right: auto;
+    margin-left: auto;
+  }
+  .header-box-logo__link {
     display: inline-block;
     color: #383735;
     vertical-align: middle;
     transition: all .3s cubic-bezier(.645,.045,.355,1);
-}
-
-.home-logo .header-box-logo-link___1Ffut img {
+  }
+  .header-box-logo__link img {
     display: inline-block;
     max-width: 266px;
     border: none;
-}
-
-.home-logo .header-box-logo___24G68 {
+  }
+ .header-box-logo__picture {
     display: -ms-flexbox;
     display: flex;
     -ms-flex-align: center;
@@ -70,5 +62,6 @@ export default {
     justify-content: center;
     height: 78px;
     margin: 15.5px 0;
+  }
 }
 </style>
