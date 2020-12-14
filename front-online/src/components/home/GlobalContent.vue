6f11efb5a0a697cdf40a75f062fafe26dfaad5ee
shiyuq <1543082564@qq.com>
2020-12-14 23:54:13 +0800

    ✨ cotnent

--- front-online/src/components/home/GlobalContent.vue
+++ front-online/src/components/home/GlobalContent.vue
@@ -1,6 +1,11 @@
 <template>
-  <header class="header-content" :class="[{'header-active__content': !isTop}]">
-  </header>
+  <div class="header-content">
+    <div class="content-ambition" :class="[{'header-active__content': !isTop}]">
+      <div class="content_text-title">
+        我们的愿景是构建未来服务业的数字化基础设施，为世界带来更多微小而美好的改变。我们不追求大，不追求强，追求成为一家能健康成长102年的好公司。
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -21,4 +26,31 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.header-content {
+  box-sizing: border-box;
+  max-width: 1350px;
+  margin-right: auto;
+  margin-left: auto;
+  padding-right: 0;
+  padding-left: 0;
+  .header-active__content {
+    margin-top: -64px;
+    background-color: #fff;
+  }
+  .content-ambition {
+    opacity: 1;
+    padding: 0 85px;
+    background-color: #fff;
+    .content_text-title {
+      background-color: #fff;
+      opacity: 1;
+      transform: translate(0px, 0px);
+      margin-bottom: 86px;
+      color: #1677ff;
+      font-size: 34px;
+      line-height: 44px;
+      word-break: break-word;
+    }
+  }
+}
 </style>
