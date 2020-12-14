9b1adc1bfc6bbb8fd01df234d30a47d9eb478cde
shiyuq <1543082564@qq.com>
2020-12-14 19:07:13 +0800

    ✨ content home

---
+++ front-online/src/components/home/GlobalContent.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="header-content" :class="[{'header-active__content': !isTop}]">
+  </header>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      isTop: true
+    }
+  },
+  methods: {},
+  mounted () {
+    window.addEventListener('scroll', () => {
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
+      this.isTop = !scrollTop
+    }, true)
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
--- front-online/src/views/Home.vue
+++ front-online/src/views/Home.vue
@@ -1,19 +1,26 @@
 <template>
   <div>
-    <GlobalNavigation></GlobalNavigation>
-    <GlobalVideo></GlobalVideo>
+    <header>
+      <GlobalNavigation></GlobalNavigation>
+      <GlobalVideo></GlobalVideo>
+    </header>
+    <section class="content">
+      <GlobalContent></GlobalContent>
+    </section>
   </div>
 </template>
 
 <script>
 import GlobalNavigation from '@/components/home/GlobalNavigation'
 import GlobalVideo from '@/components/home/GlobalVideo'
+import GlobalContent from '@/components/home/GlobalContent'
 
 export default {
   name: 'HomePage',
   components: {
     GlobalNavigation,
-    GlobalVideo
+    GlobalVideo,
+    GlobalContent
   },
   data () {
     return {}
