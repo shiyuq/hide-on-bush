408ba0c053f85b278748db7c7da0112554090dc8
shiyuq <1543082564@qq.com>
2020-12-16 18:57:48 +0800

    ✨ content complete

--- front-online/src/components/home/GlobalContent.vue
+++ front-online/src/components/home/GlobalContent.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="header-content">
     <div class="content-ambition" :class="[{'header-active__content': !isTop}]">
-      <div class="content_text-title">
-        我们的愿景是构建未来服务业的数字化基础设施，为世界带来更多微小而美好的改变。我们不追求大，不追求强，追求成为一家能健康成长102年的好公司。
+      <div class="content_text-box">
+        <div class="content_text-title">
+          我们的愿景是构建未来服务业的数字化基础设施，为世界带来更多微小而美好的改变。我们不追求大，不追求强，追求成为一家能健康成长102年的好公司。
+        </div>
+        <div class="content_text-wish">
+          我们希望：<br>
+          每一个个体可以享受到普惠、绿色的金融服务；<br>
+          每一家小微企业拥有平等的发展机会；<br>
+          通过开放合作，让数字生活触手可及。<br>
+        </div>
       </div>
     </div>
   </div>
@@ -34,14 +42,22 @@ export default {
   padding-right: 0;
   padding-left: 0;
   .header-active__content {
-    margin-top: -64px;
+    margin-top: -55px;
     background-color: #fff;
   }
   .content-ambition {
-    opacity: 1;
-    padding: 0 85px;
-    background-color: #fff;
+    display: flex;
+    padding-right: 0;
+    padding-left: 0;
+    margin-bottom: 80px;
+    flex-flow: row wrap;
+    transition: all .5s cubic-bezier(0.645, 0.045, 0.355, 1);
+    // cubic-bezier(0.68, -0.55, 0.27, 1.55) 缓冲效果
+    .content_text-box {
+      width: 100%;
+    }
     .content_text-title {
+      padding: 55px 85px 0 85px;
       background-color: #fff;
       opacity: 1;
       transform: translate(0px, 0px);
@@ -50,6 +66,13 @@ export default {
       font-size: 34px;
       line-height: 44px;
       word-break: break-word;
+    }
+    .content_text-wish {
+      padding: 0 85px;
+      color: #000;
+      font-weight: 500;
+      font-size: 26px;
+      line-height: 38px;
     }
   }
 }
