afdfcf02fd6c0951812f4787e041d4ded34442cd
hww <945247329@qq.com>
2021-01-28 21:45:58 +0800

    🐛 fix bug

--- front-online/src/views/About.vue
+++ front-online/src/views/About.vue
@@ -1,9 +1,5 @@
 <template>
   <div class="about">
-<<<<<<< HEAD
     <h1>This is an about page now</h1>
-=======
-    <h1>This is an page1</h1>
->>>>>>> f7fd01376aa2b3f673a54b14cb13736e5c26e5d7
   </div>
 </template>
