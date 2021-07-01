2e9b80c37a0c64d043e0db53fefd0b8c6f6a7428
hww <71560358+heweiwei1@users.noreply.github.com>
2021-07-01 14:37:39 +0800

    🎨 home (#23)

---
+++ front-online/src/views/navigationPages/Global.vue
@@ -0,0 +1,564 @@
+<template>
+  <div>
+    <header>
+      <GlobalNavigation></GlobalNavigation>
+    </header>
+    <div class="header">
+      <div class="pic">
+        <span class="title">
+          全球普惠
+        </span>
+        <div class="header-description">
+          <h2>支付宝在全球范围内与合作伙伴们分享技术，共同为用<br/>户提供移动支付和数字普惠金融服务。</h2>
+        </div>
+      </div>
+    </div>
+    <div class="picture-story">
+      <div class="section-header">
+        <div class="section-header-title">
+          <h1 class="section-title">图片故事</h1>
+        </div>
+      </div>
+      <div class="section-children">
+        <div class="section-children-content">
+        </div>
+        <h3>2019年8月27日</h3>
+        <div class="explanation">
+          支付宝蚂蚁森林诞生三周年，5亿蚂蚁森林用户累计碳减排792万吨，共同在地球上种下了1.22亿棵真树。受此启发，2019年7月“菲律宾版蚂蚁森林”Gcash Forest上线。上线一个多月就吸引到70多万菲律宾用户积极参与。他们在菲律宾版“支付宝”Gcash上种树，最终也将在菲律宾种下自己的 “蚂蚁森林”。
+        </div>
+      </div>
+    </div>
+    <div class="world-pay">
+      <div class="section-header">
+        <div class="section-header-title">
+          <h1 class="section-title">全球付</h1>
+        </div>
+      </div>
+      <div class="section-children">
+        <div class="section-children-content">
+          <p>
+            支付宝与全球金融机构合作，支持商家在速卖通、Lazada、Daraz和天猫海外等主流国际电商平台服务全球消费者，实现全球付、全球买、全球卖，成为世界电子贸易平台（eWTP）的重要支撑。
+          </p>
+        </div>
+        <div class="section-children-picture">
+        </div>
+      </div>
+    </div>
+    <div class="pay-world">
+      <div class="section-header">
+        <div class="section-header-title">
+          <h1 class="section-title">付全球</h1>
+        </div>
+      </div>
+      <div class="section-children">
+        <div class="section-children-content">
+          <p>
+            支付宝的蓝色二维码，连接起全球出境游客和当地商家，让中国游客可在出境时用支付宝消费，也能让国际游客在访问中国时可通过支付宝享受移动支付带来的便利。
+          </p>
+        </div>
+      </div>
+      <div class="left">
+        <div class="left-one">
+        </div>
+        <h3>在境外服务中国客户</h3>
+        <div class="explation">
+          支付宝的境外线下扫码支付，已经接入衣食住行游购等出境游场景中的各类海外商家。
+        </div>
+      </div>
+      <div class="right">
+        <div class="right-one">
+        </div>
+        <h3>在中国服务国际访客</h3>
+        <div class="explation">
+          外国人来中国，只需下载支付宝海外版，绑定自己的海外信用卡，即可在中国畅享移动支付的便利。
+        </div>
+      </div>
+    </div>
+    <div class="digital-financial">
+      <div class="section-header">
+        <div class="section-header-title">
+          <h1 class="section-title">全球数字普惠金融</h1>
+        </div>
+        <p class="section-description">通过分享支付宝的技术和行业专长，帮助合作伙伴培养本地人才，助力当地普惠金融的发展。</p>
+      </div>
+      <div class="section-children">
+        <div class="section-flex">
+          <div class="left">
+            <div class="left-one option">
+              <p class="title">印度 Paytm</p>
+              <div class="expand">+</div>
+            </div>
+            <div class="left-two option">
+              <p class="title">巴基斯坦 Easypaisa</p>
+              <div class="expand">+</div>
+            </div>
+            <div class="left-three option">
+              <p class="title">孟加拉国 bKash</p>
+              <div class="expand">+</div>
+            </div>
+            <div class="left-four option">
+              <p class="title">泰国 TrueMoney</p>
+              <div class="expand">+</div>
+            </div>
+            <div class="left-five option">
+              <p class="title">菲律宾 GCash</p>
+              <div class="expand">+</div>
+            </div>
+          </div>
+          <div class="right">
+            <div class="right-one option">
+              <p class="title">印度尼西亚 DANA</p>
+              <div class="expand">+</div>
+            </div>
+            <div class="right-two option">
+              <p class="title">马来西亚 TnGD</p>
+              <div class="expand">+</div>
+            </div>
+            <div class="right-three option">
+              <p class="title">韩国 kakaopay</p>
+              <div class="expand">+</div>
+            </div>
+            <div class="right-four option">
+              <p class="title">中国香港 AlipayHK</p>
+              <div class="expand">+</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="inclusive-plan">
+      <div class="left">
+      </div>
+      <div class="right">
+        <div class="slick-list">
+          <h2>10x1000科技普惠计划</h2>
+          <div class="example">
+            <p>2018年10月，支付宝与世界银行成员国际金融公司(IFC)共同宣布启动10x1000科技普惠计划。</p>
+            <p>我们的目标是帮助新兴市场培养技术领导者，使其成为推动数字经济发展的驱动力。</p>
+            <p>我们旨在未来10年，每年为公共和私营部门培训1000名科技领军者。这也是帮助新兴市场增加人口资本红利的重要举措。</p>
+          </div>
+          <div class="more">
+            <a href="">
+              <span class="more-text">了解更多</span>
+              <span class="arrow"></span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import GlobalNavigation from '@/components/home/GlobalNavigation'
+export default {
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  components: {
+    GlobalNavigation
+  }
+}
+</script>
+
+<style>
+a{
+  color:#383735;
+  text-decoration: none;
+  font-size: 18px;
+  line-height: 30px;
+}
+a:hover{
+  color:#383735
+}
+.header{
+  margin-top: 95px;
+}
+.header .pic {
+  position: relative;
+  background-image: url('../../assets/gobalPratt.jpeg');
+  background-size: cover;
+  background-repeat: no-repeat;
+  width: 100%;
+  height: 320px;
+}
+
+.pic .title {
+  position: relative;
+  left: 105px;
+  top: 100px;
+  color: #ffffff;
+  font-weight: 300;
+  font-size: 60px;
+  line-height: 80px;
+  text-align: left;
+  min-width: 1350px;
+}
+
+.header .pic::after {
+  position: absolute;
+  content: '';
+  background: url('../../assets/dot.png') repeat;
+  width: 100%;
+  height: 124px;
+  bottom: -128px;
+  left: 0;
+}
+
+.header-description {
+  position: absolute;
+  left: 60px;
+  top: 300px;
+  padding: 35px 62px;
+  margin-top: -30px;
+  background: #ffffff;
+  z-index: 1;
+}
+
+.header-description h2 {
+  font-size: 34px;
+  font-weight: 300;
+  line-height: 50px;
+  padding: 20px 50px;
+  margin-top: 28px;
+}
+.picture-story {
+  position: relative;
+  margin-top: 192px;
+  padding-bottom: 50px;
+  max-width: 1350px;
+  margin-right: auto;
+  margin-left: auto;
+}
+
+.section-header {
+  position: absolute;
+  left: 170px;
+  margin-bottom: 30px;
+  padding-top: 3px;
+}
+
+.section-title {
+  position: relative;
+  margin-top: 0;
+  margin-bottom: 0;
+  padding-top: 15px;
+  font-weight: 400;
+  font-size: 26px;
+}
+
+.section-title::before {
+  position: absolute;
+  content: '';
+  top: 1px;
+  left: 0;
+  width: 96px;
+  height: 1px;
+  background: #1677ff;
+}
+
+.picture-story .section-children {
+  position: absolute;
+  left: 170px;
+  right: 270px;
+  top: 100px;
+}
+
+.picture-story .section-children-content {
+  padding-top: 50%;
+  background-image: url('../../assets/AntForest.jpg');
+  background-position:50%;
+  background-size: 100%;
+}
+.picture-story .section-children h3{
+  margin-top: 10px;
+  margin-bottom: 4px;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 35px;
+  color: #1677ff;
+}
+.world-pay {
+  position: relative;
+  margin-top: 700px;
+  padding-bottom: 50px;
+  max-width: 1350px;
+  margin-right: auto;
+  margin-left: auto;
+  }
+
+.world-pay .section-children {
+  position: absolute;
+  left: 170px;
+  right: 270px;
+  top: 70px;
+}
+
+.world-pay .section-children-content p {
+  margin-top: 0;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 30px;
+}
+.world-pay .section-children .section-children-picture{
+  margin-top: 30px;
+  padding-top: 50%;
+  background-image: url('../../assets/WorldPlay.jpg');
+  background-position: 50%;
+  background-size: 100%;
+}
+.pay-world{
+  position: relative;
+  top: 650px;
+}
+.pay-world .section-children {
+  position: absolute;
+  left: 170px;
+  right: 270px;
+  top: 100px;
+}
+
+.pay-world .section-children-content p {
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 30px;
+  margin-top: -25px;
+}
+.pay-world .left{
+  position: absolute;
+  left: 170px;
+  top: 160px;
+  width: 378.25px;
+}
+.pay-world .left .left-one{
+  padding: 240px 0 0;
+  background-image: url('../../assets/oversea.jpg');
+  background-position: 50%;
+  background-size:115%;
+  margin-bottom: 16px;
+}
+.pay-world .right{
+  position: absolute;
+  left: 50%;
+  top: 160px;
+  width: 378.25px;
+}
+.pay-world .right .right-one{
+  padding: 240px 0 0;
+  background-image: url('../../assets/InternationalVisitors.jpg');
+  background-position: 50%;
+  background-size:100%;
+  margin-bottom: 16px;
+}
+.pay-world h3{
+  margin-bottom: 2px;
+  color: #1677ff;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 35px;
+}
+.digital-financial {
+  position: relative;
+  margin-top: 1290px;
+  padding-bottom: 50px;
+  max-width: 1350px;
+  margin-right: auto;
+  margin-left: auto;
+}
+
+.section-flex {
+  position: absolute;
+  width: 65%;
+  top: 130px;
+  left: 170px;
+  right: 270px;
+}
+.section-description{
+  margin-top: 18px;
+}
+.left {
+  position: absolute;
+  left: 0;
+  width: 50%;
+}
+.left .expand {
+  position: absolute;
+  top: 23px;
+  right: 29px;
+  font-weight: 400;
+  font-size: 26px;
+}
+
+.left-one {
+  margin-bottom: 12px;
+  background: #f7f8f8;
+  padding: 24px 82px 24px 24px;
+}
+
+.left-two {
+  margin-bottom: 12px;
+  background: #f7f8f8;
+  padding: 24px 82px 24px 24px;
+}
+
+.left-two .expand {
+  top: 113px;
+}
+
+.left-three {
+  margin-bottom: 12px;
+  background: #f7f8f8;
+  padding: 24px 82px 24px 24px;
+}
+
+.left-three .expand {
+  top: 201px;
+}
+.left-four {
+  margin-bottom: 12px;
+  background: #f7f8f8;
+  padding: 24px 82px 24px 24px;
+}
+
+.left-four .expand {
+  top: 292px;
+}
+.left-five {
+  margin-bottom: 12px;
+  background: #f7f8f8;
+  padding: 24px 82px 24px 24px;
+}
+
+.left-five .expand {
+  top: 381px;
+}
+.right {
+  position: absolute;
+  right: 0;
+}
+.right .expand {
+  position: absolute;
+  top: 23px;
+  right: 25px;
+  font-weight: 400;
+  font-size: 26px;
+}
+
+.right-one {
+  background: #f7f8f8;
+  padding: 35.25px 220px 35.25px 24px;
+  margin-bottom: 12px;
+}
+
+.right-two {
+  background: #f7f8f8;
+  padding: 35.25px 220px 35.25px 24px;
+  margin-bottom: 12px;
+}
+
+.right-two .expand {
+  top: 113px;
+}
+
+.right-three {
+  background: #f7f8f8;
+  padding: 35.25px 220px 35.25px 24px;
+  margin-bottom: 12px;
+}
+
+.right-three .expand {
+  top: 201px;
+}
+.right-four {
+  background: #f7f8f8;
+  padding: 35.25px 220px 35.25px 24px;
+}
+
+.right-four .expand {
+  top: 292px;
+}
+.option:hover {
+  color: #ffffff;
+  background-color: #1677ff;
+}
+.inclusive-plan{
+  margin-top: 670px;
+  position: relative;
+}
+.inclusive-plan .left{
+  width: 663px;
+  height: 596px;
+  background-image: url('../../assets/plan.jpg');
+  background-position: top;
+  background-size:cover;
+}
+.inclusive-plan .right{
+  position: absolute;
+  right: 0;
+  top: 45px;
+  width: 600px;
+  height: 510px;
+  background-color: #f7f8f8;
+}
+.inclusive-plan .right .slick-list{
+  padding: 100px 90px;
+}
+.inclusive-plan .right .slick-list h2{
+  margin-bottom: 16px;
+  color: #383735;
+  font-weight: 400;
+  font-size: 30px;
+  line-height: 35px;
+}
+.inclusive-plan .right .slick-list .example{
+  color: #383735;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 35px;
+  overflow:hidden;/*超出隐藏*/
+  text-overflow:ellipsis;/*文本溢出时显示省略标记*/
+  display:-webkit-box;/*设置弹性盒模型*/
+  -webkit-line-clamp: 6;
+  -webkit-box-orient: vertical;
+}
+.slick-list .more a{
+  display: inline-block;
+  margin-top: 32px;
+}
+.slick-list .more .more-text{
+  font-weight: 300;
+  font-size: 15px;
+  line-height: 30px;
+  margin-right: 20px;
+}
+.slick-list .arrow{
+  position: relative;
+  display: inline-block;
+  width: 35px;
+  height: 6px;
+  box-shadow: inset 0 0 0 2px;
+  border-top: 2px solid transparent;
+  border-bottom: 2px solid transparent;
+  color: #1677ff;
+  transition: all 0.3s;
+}
+.slick-list .arrow:hover{
+  width: 50px;
+}
+.slick-list .arrow::after{
+  position: absolute;
+  content: '';
+  width: 10px;
+  height: 10px;
+  border-top: 2px solid;
+  border-right: 2px solid;
+  transform: rotate(45deg);
+  right: 0;
+  bottom: -4px;
+}
+</style>
---
+++ front-online/src/views/navigationPages/OpenPlatform.vue
@@ -0,0 +1,292 @@
+<template>
+  <div>
+    <header>
+      <GlobalNavigation></GlobalNavigation>
+    </header>
+    <div class="header">
+      <div class="pic">
+        <span class="title">
+          平台开放
+        </span>
+        <div class="header-description">
+          <h2>我们一直致力于打造一个开放的生态系统，从业务开<br/>放、能力开放、技术开放三方面携手合作伙伴向商家和<br/>消费者提供最好的服务，共同推动行业变革，为社会创<br/>造更大的价值。</h2>
+        </div>
+      </div>
+    </div>
+    <div class="alipay">
+      <div class="left">
+      </div>
+      <div class="right">
+        <div class="slick-list">
+          <h2>支付宝开放平台</h2>
+          <div class="example">
+            我们基于支付宝海量用户，以小程序为主要载体，将强大的支付、营销、安全能力开放给第三方合作伙伴，帮助其创建更具竞争力的应用，让用户获得更丰富的体验，让平台生态更加繁荣，最终实现多方共赢。
+          </div>
+          <div class="more">
+            <a href="">
+              <span class="more-text">了解更多</span>
+              <span class="arrow"></span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="global-business">
+      <div class="left">
+        <div class="slick-list">
+          <h2>全球商家开放平台</h2>
+          <div class="example">
+            与合作伙伴一起，我们为全球商家提供线上和线下多场景，多行业的数字技术解决方案，包括数字支付、数字营销和运营，支持商家更好的服务消费者。
+          </div>
+          <div class="more">
+            <a href="">
+              <span class="more-text">了解更多</span>
+              <span class="arrow"></span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="right">
+      </div>
+    </div>
+    <div class="ant-chain">
+      <div class="left">
+      </div>
+      <div class="right">
+        <div class="slick-list">
+          <h2>蚂蚁链开放联盟链</h2>
+          <div class="example">
+            这是一个低成本、低门槛开放普惠的区块链服务网络，将企业区块链开发门槛从百万级降低至千元，缩短建链时间至最快一分钟。开放联盟链还与中小企业共享蚂蚁区块链丰富的场景解决方案，助力各行业区块链商业场景快速落地。
+          </div>
+          <div class="more">
+            <a href="">
+              <span class="more-text">了解更多</span>
+              <span class="arrow"></span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import GlobalNavigation from '@/components/home/GlobalNavigation'
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {
+  },
+  components: {
+    GlobalNavigation
+  }
+}
+</script>
+
+<style>
+body{
+  height: 2000px;
+}
+a{
+  color:#383735;
+  text-decoration: none;
+  font-size: 18px;
+  line-height: 30px;
+}
+a:hover{
+  color:#383735
+}
+.header{
+  margin-top: 95px;
+}
+.header .pic {
+  position: relative;
+  background-image: url('../../assets/OpenPlatform.jpeg');
+  background-size: cover;
+  background-repeat: no-repeat;
+  width: 100%;
+  height: 320px;
+}
+
+.pic .title {
+  position: relative;
+  left: 105px;
+  top: 100px;
+  color: #ffffff;
+  font-weight: 300;
+  font-size: 60px;
+  line-height: 80px;
+  text-align: left;
+  min-width: 1350px;
+}
+
+.header .pic::after {
+  position: absolute;
+  content: '';
+  background: url('../../assets/dot.png') repeat;
+  width: 100%;
+  height: 124px;
+  bottom: -128px;
+  left: 0;
+}
+
+.header-description {
+  position: absolute;
+  left: 60px;
+  top: 300px;
+  padding: 35px 62px;
+  margin-top: -30px;
+  background: #ffffff;
+  z-index: 1;
+}
+
+.header-description h2 {
+  font-size: 34px;
+  font-weight: 300;
+  line-height: 50px;
+  padding: 20px 50px;
+  margin-top: 28px;
+}
+.alipay{
+  margin-top: 350px;
+  position: relative;
+}
+.alipay .left{
+  width: 663px;
+  height: 596px;
+  background-image: url('../../assets/alipay.jpeg');
+  background-position: top;
+  background-size:cover;
+}
+.alipay .right{
+  position: absolute;
+  right: 0;
+  top: 45px;
+  width: 600px;
+  height: 510px;
+  background-color: #f7f8f8;
+}
+.alipay .right .slick-list{
+  padding: 100px 90px;
+}
+.alipay .right .slick-list h2{
+  margin-bottom: 16px;
+  color: #383735;
+  font-weight: 400;
+  font-size: 30px;
+  line-height: 35px;
+}
+.alipay .right .slick-list .example{
+  color: #383735;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 35px;
+}
+.slick-list .more a{
+  display: inline-block;
+  margin-top: 32px;
+}
+.slick-list .more .more-text{
+  font-weight: 300;
+  font-size: 15px;
+  line-height: 30px;
+  margin-right: 20px;
+}
+.slick-list .arrow{
+  position: relative;
+  display: inline-block;
+  width: 35px;
+  height: 6px;
+  box-shadow: inset 0 0 0 2px;
+  border-top: 2px solid transparent;
+  border-bottom: 2px solid transparent;
+  color: #1677ff;
+  transition: all 0.3s;
+}
+.slick-list .arrow:hover{
+  width: 50px;
+}
+.slick-list .arrow::after{
+  position: absolute;
+  content: '';
+  width: 10px;
+  height: 10px;
+  border-top: 2px solid;
+  border-right: 2px solid;
+  transform: rotate(45deg);
+  right: 0;
+  bottom: -4px;
+}
+.global-business{
+  margin-top: 160px;
+  position: relative;
+}
+.global-business .right{
+  position: absolute;
+  right:0;
+  top: -37px;
+  width: 663px;
+  height: 596px;
+  background-image: url('../../assets/global-business.jpeg');
+  background-position: top;
+  background-size:cover;
+}
+.global-business .left{
+  width: 600px;
+  height: 510px;
+  background-color: #f7f8f8;
+}
+.global-business .left .slick-list{
+  padding: 100px 90px;
+}
+.global-business .left .slick-list h2{
+  margin-bottom: 16px;
+  color: #383735;
+  font-weight: 400;
+  font-size: 30px;
+  line-height: 35px;
+}
+.global-business .left .slick-list .example{
+  color: #383735;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 35px;
+}
+.ant-chain{
+  margin-top: 180px;
+  position: relative;
+}
+.ant-chain .left{
+  width: 663px;
+  height: 596px;
+  background-image: url('../../assets/ant-chain.jpeg');
+  background-position: top;
+  background-size:cover;
+}
+.ant-chain .right{
+  position: absolute;
+  right: 0;
+  top: 45px;
+  width: 600px;
+  height: 510px;
+  background-color: #f7f8f8;
+}
+.ant-chain .right .slick-list{
+  padding: 100px 90px;
+}
+.ant-chain .right .slick-list h2{
+  margin-bottom: 16px;
+  color: #383735;
+  font-weight: 400;
+  font-size: 30px;
+  line-height: 35px;
+}
+.ant-chain .right .slick-list .example{
+  color: #383735;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 35px;
+}
+</style>
